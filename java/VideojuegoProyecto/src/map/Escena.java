fa24f09abfb066efa2e616f7cd107f57616e63fe
Unidavid <david.gomezz@edu.uah.es>
2019-05-02 21:35:31 +0200

    Se ha añadido el arrayList de enemigos indicando la cantidad que hay en cada una de las escenas

--- java/VideojuegoProyecto/src/map/Escena.java
+++ java/VideojuegoProyecto/src/map/Escena.java
@@ -5,9 +5,9 @@
  */
 package map;
 
+import characters.Monstruo;
 import java.util.ArrayList;
-import org.newdawn.slick.geom.Polygon;
-import org.newdawn.slick.geom.Shape;
+import org.newdawn.slick.geom.*;
 import org.newdawn.slick.tiled.TiledMap;
 
 /**
@@ -20,14 +20,16 @@ public class Escena {
     private final Polygon area_entrada;  //polígono para entrar a la escena siguiente
     private final Polygon area_salida;   //polígono para salir de la escena e ir a la anterior
     private final Polygon mapa_colision;
-    private ArrayList<Polygon> mapa_objetos;
+    private final ArrayList<Polygon> mapa_objetos;
+    private final ArrayList<Monstruo> enemigos;
 
-    public Escena(TiledMap mapa_escena,Polygon mapa_colision,ArrayList<Polygon> mapa_objetos,Polygon area_entrada,Polygon area_salida) {
+    public Escena(TiledMap mapa_escena,Polygon mapa_colision,ArrayList<Polygon> mapa_objetos,Polygon area_entrada,Polygon area_salida,ArrayList<Monstruo> enemigos) {
         this.mapa_escena = mapa_escena;
         this.mapa_colision = mapa_colision;
         this.area_entrada = area_entrada;
         this.area_salida = area_salida;
         this.mapa_objetos = mapa_objetos;
+        this.enemigos = enemigos;
     }
 
     public TiledMap getMapa_escena() {
@@ -49,6 +51,10 @@ public class Escena {
     public ArrayList<Polygon> getMapa_objetos(){
         return mapa_objetos;
     }
+
+    public ArrayList<Monstruo> getEnemigos() {
+        return enemigos;
+    }  
     
     public boolean colisionConObjetos(Shape lado){
         if(lado.intersects(mapa_colision)){
