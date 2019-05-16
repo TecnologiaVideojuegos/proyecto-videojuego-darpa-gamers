eb96847974d35056d0eb57937318787048ae3f4d
adarve99 <48323844+adarve99@users.noreply.github.com>
2019-05-16 23:54:40 +0200

    Add files via upload

---
+++ grafico/mapas/resources_tsx/casa_madera.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="casa_madera" tilewidth="32" tileheight="32" tilecount="117" columns="9">
+ <image source="../casa_madera.png" width="288" height="416"/>
+</tileset>
