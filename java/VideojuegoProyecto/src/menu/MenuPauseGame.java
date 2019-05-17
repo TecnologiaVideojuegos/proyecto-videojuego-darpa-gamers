c23fcdd2da9925313d59e3afab7cdbb85d032741
Unidavid <david.gomezz@edu.uah.es>
2019-05-17 17:07:36 +0200

    Se termina la clase menú pausa, añadiendo una transición al salir del nivel

--- java/VideojuegoProyecto/src/menu/MenuPauseGame.java
+++ java/VideojuegoProyecto/src/menu/MenuPauseGame.java
@@ -7,9 +7,13 @@ package menu;
 
 import characters.Jugador;
 import imagen.Sprite;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import location.Punto;
 import org.newdawn.slick.gui.*;
 import org.newdawn.slick.*;
+import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.state.transition.*;
 
 /**
  *
@@ -17,9 +21,9 @@ import org.newdawn.slick.*;
  */
 public class MenuPauseGame implements ComponentListener{
     
-    private Sprite fondo,continuar,salir;
-    private MouseOverArea[] botones = new MouseOverArea[2];
-    private boolean pausa,debug;
+    private Sprite fondo,continuar,controles,salir,imgControl,atras;
+    private MouseOverArea[] botones = new MouseOverArea[4];
+    private boolean pausa,debug,control;
     private int estado = -1;
     private GameContainer container;
 
@@ -27,11 +31,15 @@ public class MenuPauseGame implements ComponentListener{
         try{
             this.pausa = false;
             this.debug = false;
+            this.control = false;
             this.container = container;
-            this.fondo = new Sprite("./res/grafico/fonds/fondo.png");
-            this.continuar = new Sprite("./res/grafico/buttons/boton_inicio.png",new Punto(200,300));
-            this.salir = new Sprite("./res/grafico/buttons/boton_salir.png");
-            Sprite[] buttons = {continuar,salir};
+            this.fondo = new Sprite("./res/grafico/fonds/menu_pausa_nobotones_2.png");
+            this.continuar = new Sprite("./res/grafico/buttons/boton_continuar.png",new Punto(250,250));
+            this.controles = new Sprite("./res/grafico/buttons/boton_controles.png",new Punto(250,350));
+            this.salir = new Sprite("./res/grafico/buttons/boton_salir.png",new Punto(378,500));
+            this.atras = new Sprite("./res/grafico/buttons/boton_atras.png",new Punto(378,550));
+            this.imgControl = new Sprite("./res/grafico/game_utils/controles.png");
+            Sprite[] buttons = {continuar,controles,salir,atras};
             for(int i = 0;i<botones.length;i++){
                 botones[i] = new MouseOverArea(container,buttons[i],(int)buttons[i].getPosicion().getX(),(int)buttons[i].getPosicion().getY(),buttons[i].getWidth(),buttons[i].getHeight(),this);         
                 botones[i].setNormalColor(new Color(1,1,1,0.7f));
@@ -41,20 +49,60 @@ public class MenuPauseGame implements ComponentListener{
     }
     
     public void comprobarMenuPausa(Input entrada){
-        if(entrada.isKeyPressed(Input.KEY_ESCAPE)){
+        if(entrada.isKeyPressed(Input.KEY_ESCAPE) && !control){
             if(pausa){
                 pausa = false;
             }else{
                 pausa = true;
             }
         }
-        if(entrada.isKeyPressed(Input.KEY_F3)){
+        if(entrada.isKeyPressed(Input.KEY_F3) && !control){
             if(debug){
                 debug = false;
             }else{
                 debug = true;
             }
         }
+    }
+    
+    public void comprobarEstado(GameContainer container,StateBasedGame game) throws SlickException{
+        switch (estado) {
+            case 0:
+                //Continuar
+                estado = -1;
+                pausa = false;
+                break;
+            case 1:
+                //Controles
+                estado = -1;
+                pausa = false;
+                control = true;
+                break;
+            case 2:
+                //Salir
+                estado = -1;                  
+                try {
+                    game.enterState(0,FadeOutTransition.class.newInstance(), FadeInTransition.class.newInstance());
+                } catch (InstantiationException ex) {
+                    Logger.getLogger(MenuPauseGame.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (IllegalAccessException ex) {
+                    Logger.getLogger(MenuPauseGame.class.getName()).log(Level.SEVERE, null, ex); 
+                }
+                break;
+            case 3:
+                //Atras
+                estado = -1;
+                control = false;
+                pausa = true;
+                break;
+            default:
+                break;
+        }
+    }
+    
+    public void gestionarMenuPausa(Input entrada,GameContainer container,StateBasedGame game) throws SlickException{
+        this.comprobarMenuPausa(entrada);
+        this.comprobarEstado(container, game);
     }
     
     public void mostrarMenu(Graphics g,Jugador j){
@@ -78,14 +126,14 @@ public class MenuPauseGame implements ComponentListener{
         }
         if(pausa){
             fondo.draw();
-            for (MouseOverArea botone : botones) {
-                botone.render(container, g);
+            for(int i = 0;i<(botones.length-1);i++){
+                botones[i].render(container, g);
             }
         }
-    }
-
-    public void debug(Graphics g){
-        
+        if(control){
+            imgControl.draw();
+            botones[3].render(container, g);
+        }
     }
     
     public boolean isPausa() {
